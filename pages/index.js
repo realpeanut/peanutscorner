e0c16ae12b9de5b9c0cacfd1731225efad299753
realpeanut <58894179+realpeanut@users.noreply.github.com>
2023-12-16 10:02:27 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Peanut's corner</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Peanut's corner" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          ello 👋
         </p>
       </main>
 
